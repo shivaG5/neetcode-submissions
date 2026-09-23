3df2782b5ba4ecf60666dc2318b5f7ae8259658d
Shiva Kumar <molugushivakumar15@gmail.com>
2026-09-23 16:44:25 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int r=*max_element(piles.begin(),piles.end());
+        int ans=r;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            long long piles_ans=0;
+            for(int p:piles)
+            {
+                piles_ans+=(p+mid-1)/mid;
+            }
+            if(piles_ans<=h)
+            {
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
+};
